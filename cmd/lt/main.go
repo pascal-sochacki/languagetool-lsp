5c4fc15d2fd9457e101073a7a155671058343ade
Pascal Sochacki <pascal@sochacki.dev>
2024-01-06 11:27:47 +0100

    fix test

--- cmd/lt/main.go
+++ cmd/lt/main.go
@@ -6,10 +6,11 @@ import (
 	"os"
 
 	"github.com/pascal-sochacki/languagetool-lsp/pkg/languagetool"
+	"go.uber.org/zap"
 )
 
 func main() {
-	client := languagetool.NewClient()
+	client := languagetool.NewClient(zap.NewNop())
 	result, err := client.CheckText(context.Background(), "Das ist ein Langer Text das hat fehler hat!", "auto")
 	if err != nil {
 		fmt.Sprintln(err.Error())
